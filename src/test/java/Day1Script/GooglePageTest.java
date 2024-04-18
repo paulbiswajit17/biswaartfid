fbe4d1668e66239085162ea344949477bff295a2
paulbiswajit17 <emailid>
2024-04-18 05:37:35 +0000

    initial proj biswa-18th apr

---
+++ src/test/java/Day1Script/GooglePageTest.java
@@ -0,0 +1,16 @@
+package Day1Script;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+
+	}
+
+}
