3c6ba0b47fdc31b0d04f15bfae644dcad039e1a8
Administrator <Administrator@172.31.0.204>
2024-04-18 06:39:24 +0000

    page title

--- src/test/java/Day1Script/GooglePageTest.java
+++ src/test/java/Day1Script/GooglePageTest.java
@@ -9,6 +9,7 @@ public class GooglePageTest {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
+		System.out.println("page title is :" + driver.getTitle());
 		
 
 	}
