2288da135248b2a6beef6da114c625b4e9eaa95d
Administrator <Administrator@172.31.6.129>
2024-04-22 07:24:13 +0000

    day 3 commit

--- src/test/java/Day1Script/GooglePageTest.java
+++ src/test/java/Day1Script/GooglePageTest.java
@@ -1,15 +1,36 @@
 package Day1Script;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("page title is :" + driver.getTitle());
+		
+	//	ChromeOptions options= new ChromeOptions();
+	//	WebElement srcbox= driver.findElement(By.id("APjFqb"));
+	//	WebElement srcbox= driver.findElement(By.name("q"));
+	//	WebElement srcbox= driver.findElement(By.className("gLFyf"));
+		WebElement srcbox= driver.findElement(By.tagName("textarea"));
+		srcbox.sendKeys("Hello");
+		srcbox.sendKeys(Keys.ENTER);
+		
+		
+		
+		
+		
+		
 		
 
 	}
