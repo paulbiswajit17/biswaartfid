677e8877dea7ca9fd90b2495fbc609209f001623
Administrator <Administrator@172.31.14.95>
2024-04-24 12:01:10 +0000

    personal practice

--- src/test/java/Day1Script/DropDownTest.java
+++ src/test/java/Day1Script/DropDownTest.java
@@ -2,6 +2,7 @@ package Day1Script;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -17,11 +18,16 @@ public class DropDownTest {
 	{
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demo.seleniumeasy.com/basic-select-dropdown-demo.html");
+		driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+		
+		WebElement c= driver.findElement(By.xpath("//input[@id='user-message']"));
+		c.sendKeys("hello");
+		WebElement d= driver.findElement(By.xpath("//*[@id=\"get-input\"]/button"));
+		d.click();		
 		Select a = new Select(driver.findElement(By.id("select-demo")));
 		a.selectByValue("Tuesday");
 		
-		Select b = new Select(driver.findElement(By.id("multi-select")));
+		/* Select b = new Select(driver.findElement(By.id("multi-select")));
 		
 		if(b.isMultiple())
 		{
@@ -30,6 +36,7 @@ public class DropDownTest {
 			b.selectByVisibleText("Washington");
 		}
 		
+*/		
 
 	}
 
